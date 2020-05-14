18d09320f2db4b2e56aad910ddfc1236f25c9441
Alexey <charmedbns@gmail.com>
2020-05-14 20:35:00 +0300

    Lesson 27. Map View Controller

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Лёпа on 14.05.2020.
+//  Copyright © 2020 Lepa. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func closeVC() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
